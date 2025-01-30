9fab8e21369ae2d2b069dd68b3d59fd256fecb01
tourecodeur <touremacoumba682@gmail.com>
2025-01-30 23:25:05 +0000

    macoumba 30/01/25

--- src/app/modules/client/annonces/annonces.component.ts
+++ src/app/modules/client/annonces/annonces.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-annonces',
   standalone: true,
-  imports: [],
+  imports: [    
+    BrowserModule,
+    FormsModule ],
   templateUrl: './annonces.component.html',
   styleUrl: './annonces.component.css'
 })
 export class AnnoncesComponent {
-
+  // Liste des annonces
 }
